29121f69ec4c4769eda1fa65e4ab6d9e15d228e3
Carlos Treviño <carloscharlie4td@hotmail.com>
2022-03-16 21:53:29 -0500

    Se agrego el idioma ingles y que poder cambiarlos

--- JavaScript/Js.js
+++ JavaScript/Js.js
@@ -3,6 +3,123 @@ var SeccionesOl = document.querySelector('.Ol');
 var TarjetaSeccion = []; 
 var IconoServicio = []; 
 var TituloServicioTarjeta = []; 
+var PresentarT = document.querySelector("#PresentarT");
+var BanderaMexico = document.querySelector('#BanderaMx');
+var BanderaUS = document.querySelector('#BanderaUs');
+var TextoPrueba = document.querySelector("#TextoPrueba");
+var SobreMiT = document.querySelector("#SobreMiT");
+var ServiciosT = document.querySelector("#ServiciosT");
+var TecnologiasT = document.querySelector("#TecnologiasT");
+var ProyectosT = document.querySelector("#ProyectosT");
+var ContactoT = document.querySelector("#ContactoT");
+var SobreMiTT = document.querySelector("#SobreMiTT");
+var ServiciosTT = document.querySelector("#ServiciosTT");
+var TecnologiasTT = document.querySelector("#TecnologiasTT");
+var ProyectosTT = document.querySelector("#ProyectosTT");
+var ContactoTT = document.querySelector("#ContactoTT");
+var TextoSobreMiT = document.querySelector("#TextoSobreMiT");
+var DisenoIntuitivoT = document.querySelector(".DisenoIntuitivoT");
+var RendimientoT = document.querySelector(".RendimientoT");
+var ResponsivoT = document.querySelector(".ResponsivoT");
+var CalidadT = document.querySelector(".CalidadT");
+var DisenoLlamativoT = document.querySelector(".DisenoLlamativoT");
+var TextoTecnologiasT = document.querySelector("#TextoTecnologiasT");
+var NombreT = document.querySelector("#NombreT");
+var NumeroT = document.querySelector("#NumeroT");
+var MensajeT = document.querySelector("#MensajeT");
+var SubmitT = document.querySelector("#SubmitT");
+var CurriculumT = document.querySelector("#CurriculumT");
+var TextoServicioT1 = document.querySelector("#TextoServicioT1");
+var TextoServicioT2 = document.querySelector("#TextoServicioT2");
+var TextoServicioT3 = document.querySelector("#TextoServicioT3");
+var TextoServicioT4 = document.querySelector("#TextoServicioT4");
+var TextoServicioT5 = document.querySelector("#TextoServicioT5");
+var TextoServicioT6 = document.querySelector("#TextoServicioT6");
+var NombreCT = document.querySelector("#Nombre");
+var MensajeCT = document.querySelector("#Mensaje");
+
+function LenguaMxF()
+{
+	PresentarT.innerHTML = "Hola me presento, soy";
+	SobreMiT.innerHTML = "Sobre mi";
+	ServiciosT.innerHTML = "Servicios";
+	TecnologiasT.innerHTML = "Tecnologias";
+	ProyectosT.innerHTML = "Proyectos";
+	ContactoT.innerHTML = "Contacto";
+	SobreMiTT.innerHTML = "Sobre mi";
+	ServiciosTT.innerHTML = "Servicios";
+	TecnologiasTT.innerHTML = "Tecnologias";
+	ProyectosTT.innerHTML = "Proyectos";
+	ContactoTT.innerHTML = "Contacto";
+	TextoPrueba.innerHTML = "Desarrollador frontend enfocado en crear la mejor experiencia para el usuario.<br/>Conozco lo necesario sobre diseño y backend para que mis paginas cumplan los objetivos deseados.";
+	TextoSobreMiT.innerHTML = "Soy Carlos, un programador autodidacta, aprendi a programar en C++, programe videojuegos para Unity3D en C# y actualmente me dedico al desarrollo Front End.<br><br>Empeze a programar porque es un area que me interesa y apasiona, soy una persona a la cual le gusta aprender, me gusta mucho resolver todos los desafios que te encuentras a la hora de programar.<br><br>Soy bilingue, alguien sociable al que le gustan las artes marciales, los gatos, las redes sociales, con muchas ganas de aprender y mejorar para lograr mis metas siendo feliz en el proceso.";
+	DisenoIntuitivoT.innerHTML = "Diseño intuitivo";
+	RendimientoT.innerHTML = "Rendimiento";
+	ResponsivoT.innerHTML = "Responsivo";
+	CalidadT.innerHTML = "Calidad";
+	DisenoLlamativoT.innerHTML = "Diseño llamativo";
+	TextoTecnologiasT.innerHTML = "Cuento con los conocimientos sobre backend para comunicarme eficientemente con el.";
+	TextoServicioT1.innerHTML = "Mis desarrollos web tienen un diseño intuitivo y simple.";
+	TextoServicioT2.innerHTML = "Tu sitio web estara optimizado de la mejor manera.";
+	TextoServicioT3.innerHTML = "Mis desarrollos web siempre cuentan con las estrategias SEO.";
+	TextoServicioT4.innerHTML = "Tu sitio web se adaptara a la resolución de cualquier dispositivo.";
+	TextoServicioT5.innerHTML = "Mi desarrollo web garantiza la calidad de la pagina.";
+	TextoServicioT6.innerHTML = "Tu sitio web contara con un diseño atractivo para los usuarios.";
+	NombreT.innerHTML = "Nombre *";
+	NumeroT.innerHTML = "Numero";
+	MensajeT.innerHTML = "Mensaje *";
+	SubmitT.innerHTML = "Enviar mensaje";
+	CurriculumT.innerHTML = "Curriculum";
+	NombreCT.placeholder = "Nombre";
+	MensajeCT.placeholder = "Escribe tu mensaje aqui";
+}
+
+function LenguaUsF()
+{
+	PresentarT.innerHTML = "Hi, my name is";
+	SobreMiT.innerHTML = "About me";
+	ServiciosT.innerHTML = "Services";
+	TecnologiasT.innerHTML = "Technologies";
+	ProyectosT.innerHTML = "Projects";
+	ContactoT.innerHTML = "Contact";
+	SobreMiTT.innerHTML = "About me";
+	ServiciosTT.innerHTML = "Services";
+	TecnologiasTT.innerHTML = "Technologies";
+	ProyectosTT.innerHTML = "Projects";
+	ContactoTT.innerHTML = "Contact";
+	TextoPrueba.innerHTML = "FrontEnd Developer specialized in giving the best user experience.<br/>I have basic knowledge about design and backend so that my pages meet the desired objectives.";
+	TextoSobreMiT.innerHTML = "I am Carlos, a self-taught programmer, i learned to program in C++, i programed videogames for Unity3D in C# and i am currently dedicated to Front End development.<br><br>I started programming because it is an area that interests me and i am passionate about, i am a person who likes to learn and have challengues, i love solving all the problems that i face when i am programming.<br><br>I am bilingual, someone sociable who likes martial arts, cats, social networks, eager to learn and improve to achieve my goals, being happy in the process.";
+	DisenoIntuitivoT.innerHTML = "Intuitive design";
+	RendimientoT.innerHTML = "Performance";
+	ResponsivoT.innerHTML = "Responsive";
+	CalidadT.innerHTML = "Quality";
+	DisenoLlamativoT.innerHTML = "Striking design";
+	TextoTecnologiasT.innerHTML = "I have the knowledge about backend to communicate efficiently with it.";
+	TextoServicioT1.innerHTML = "My web developments have an intuituve and simple desing.";
+	TextoServicioT2.innerHTML = "Your website will be optimized in the best way.";
+	TextoServicioT3.innerHTML = "My web developments always have the SEO strategies.";
+	TextoServicioT4.innerHTML = "Your website will adapt to the resolution of any device.";
+	TextoServicioT5.innerHTML = "My web developments guarantees the website quality.";
+	TextoServicioT6.innerHTML = "Your website will have an attractive design for users.";
+	NombreT.innerHTML = "Name *";
+	NumeroT.innerHTML = "Number";
+	MensajeT.innerHTML = "Message *";
+	SubmitT.innerHTML = "Send message";
+	CurriculumT.innerHTML = "Resume";
+	NombreCT.placeholder = "Name";
+	MensajeCT.placeholder = "Type your message here";
+}
+
+if(localStorage.getItem("Lengua"))
+{
+	let Lengua = localStorage.getItem("Lengua");
+	Lengua == "MX" ? LenguaMxF() : LenguaUsF() ;
+}
+else
+{
+	localStorage.setItem("Lengua","MX");
+	LenguaMxF();
+}
 
 function AnimarServicios(_NumeroServicio)
 {
@@ -31,6 +148,18 @@ for(var i = 1 ; i < 7 ; i++)
 	TituloServicioTarjeta[i] = document.getElementById("" + i + "2");
 	AnimarServicios(i);
 }
+
+BanderaMexico.addEventListener('click', () =>
+{
+	localStorage.setItem("Lengua","MX");
+	LenguaMxF();
+});
+
+BanderaUS.addEventListener('click', () =>
+{
+	localStorage.setItem("Lengua","US");
+	LenguaUsF();
+});
 
 MenuBoton.addEventListener('click', () =>
 {
--- index.php
+++ index.php
@@ -15,26 +15,26 @@
 		<nav class="Nav">
 			
 				<div class="A">
-						<img src="BanderasSvg/mx.svg" class="Bandera">
-						<img src="BanderasSvg/us.svg" class="Bandera BDerecha">
+						<img src="BanderasSvg/mx.svg" class="Bandera" id="BanderaMx">
+						<img src="BanderasSvg/us.svg" class="Bandera BDerecha" id="BanderaUs">
 				</div>
 			
 			<div class="SeccionesMenu">
 				<ol class="Ol TaparPoner">
 					<li class="Li">
-						<a class="ASecciones" href="#SobreMi">Sobre mi</a>
+						<a class="ASecciones" href="#SobreMi" id="SobreMiT"></a>
 					</li>
 					<li class="Li">
-						<a class="ASecciones" href="#Servicios">Servicios</a>
+						<a class="ASecciones" href="#Servicios" id="ServiciosT"></a>
 					</li>
 					<li class="Li">
-						<a class="ASecciones" href="#Tecnologias">Tecnologias</a>
+						<a class="ASecciones" href="#Tecnologias" id="TecnologiasT"></a>
 					</li>
 					<li class="Li">
-						<a class="ASecciones" href="#Proyectos">Proyectos</a>
+						<a class="ASecciones" href="#Proyectos" id="ProyectosT"></a>
 					</li>
 					<li class="Li">
-						<a class="ASecciones" href="#Contacto">Contacto</a>
+						<a class="ASecciones" href="#Contacto" id="ContactoT"></a>
 					</li>
 				</ol>
 			</div>
@@ -53,7 +53,7 @@
 	</div>
 	<div orientation="left" class="AftersPadreLeft">
 		<div class="Afters">
-			<p href="" class="Izquierda">Desarrollador Front End</p>
+			<p href="" class="Izquierda">Front End Developer Jr</p>
 		</div>
 	</div>
 
@@ -61,25 +61,18 @@
 	<main class="Main">
 
 		<section class="Main__YoResumido">
-			<h2 class="PresentarNombre">Hola me presento, soy </h2>
+			<h2 class="PresentarNombre" id="PresentarT"></h2>
 			<p class="NombreTitulo">Carlos Treviño Dueñas.</p>
-			<h2 class="TextoBreveInicio">Desarrollador frontend especializado en crear la mejor experiencia para el usuario.<br>Cuento con conocimientos basicos sobre diseño y backend para que mis paginas cumplan los objetivos deseados.</h2>
+			<h2 class="TextoBreveInicio" id="TextoPrueba"></h2>
 			<i class="fal fa-laptop-code IconoInicio"></i>
 			<i class="fal fa-database IconoInicio2"></i>
 		</section>
 
 		<section class="Main__SobreMiC" id="SobreMi">
-				<h2 class="TituloSecciones">Sobre mi</h2>
+				<h2 class="TituloSecciones" id="SobreMiTT"></h2>
 				<div class="ContenedorSobreMi">
 					<div class="Texto">
-						<h4 class="TextoSobreMi">
-							Hola, me llamo Carlos y soy desarrollador frontend, aprendi a programar con C++, no solo he programado paginas web, tambien he programado videojuegos para Unity3D en C#.
-							<br>
-							<br>
-							Empeze a programar porque soy una persona a la cual le gusta aprender y le gustan los retos, me gusta mucho resolver todos los problemas que te encuentras a la hora de programar.
-							<br>
-							<br>
-							Soy una persona bilingue, una persona a la cual le encantan los deportes, una persona sociable, una persona con muchas ganas de aprender y mejorar para lograr sus metas siendo feliz en el proceso.
+						<h4 class="TextoSobreMi" id="TextoSobreMiT">
 						</h4>
 					</div>
 					<div class="ImagenMiaContenedor">
@@ -91,24 +84,24 @@
 		</section>
 
 		<section class="Main__ServiciosC" id="Servicios">
-			<h2 class="TituloSecciones">Servicios</h2>
+			<h2 class="TituloSecciones" id="ServiciosTT"></h2>
 			<div class="ContenedorServicios">
 				<div class="Servicio" id="1">
 					<div class="ServicioEnfrente">
 						<i class="fal fa-lightbulb-on IconosServicios" id="11"></i>
-						<h4 class="TituloServicioTarjeta" id="12">Diseño intuitivo</h4>
+						<h4 class="TituloServicioTarjeta DisenoIntuitivoT" id="12"></h4>
 					</div>
 					<div class="ServicioAtras">
-						<p class="TextoServicio">Aseguro un diseño con un uso facil y simple de entender.</p>
+						<p class="TextoServicio" id="TextoServicioT1"></p>
 					</div>
 				</div>
 				<div class="Servicio" id="2">
 					<div class="ServicioEnfrente">
 						<i class="fal fa-tachometer-fastest IconosServicios" id="21"></i>
-						<h4 class="TituloServicioTarjeta" id="22">Rendimiento</h4>
+						<h4 class="TituloServicioTarjeta RendimientoT" id="22"></h4>
 					</div>
 					<div class="ServicioAtras">
-						<p class="TextoServicio">El que tu pagina carge rapido asegura que tus usuarios se queden en tu pagina.</p>
+						<p class="TextoServicio" id="TextoServicioT2"></p>
 					</div>
 				</div>
 				<div class="Servicio" id="3">
@@ -117,40 +110,40 @@
 						<h4 class="TituloServicioTarjeta" id="32">SEO</h4>
 					</div>
 					<div class="ServicioAtras">
-						<p class="TextoServicio">Tu sitio web siempre estara optimizado con las estrategias SEO.</p>
+						<p class="TextoServicio" id="TextoServicioT3"></p>
 					</div>
 				</div>
 				<div class="Servicio" id="4">
 					<div class="ServicioEnfrente">
 						<i class="fal fa-phone-laptop IconosServicios" id="41"></i>
-						<h4 class="TituloServicioTarjeta" id="42">Responsivo</h4>
+						<h4 class="TituloServicioTarjeta ResponsivoT" id="42"></h4>
 					</div>
 					<div class="ServicioAtras">
-						<p class="TextoServicio">Tu sitio se adaptara a cualquier resolucion del dispositivo donde se visite.</p>
+						<p class="TextoServicio" id="TextoServicioT4"></p>
 					</div>
 				</div>
 				<div class="Servicio" id="5">
 					<div class="ServicioEnfrente">
 						<i class="fal fa-file-certificate IconosServicios" id="51"></i>
-						<h4 class="TituloServicioTarjeta" id="52">Calidad</h4>
+						<h4 class="TituloServicioTarjeta CalidadT" id="52"></h4>
 					</div>
 					<div class="ServicioAtras">
-						<p class="TextoServicio">Puedes estar seguro que mi desarrollo web garantiza la calidad de la pagina.</p>
+						<p class="TextoServicio" id="TextoServicioT5"></p>
 					</div>
 				</div>
 				<div class="Servicio" id="6">
 					<div class="ServicioEnfrente">
 						<i class="fal fa-pencil-paintbrush IconosServicios" id="61"></i>
-						<h4 class="TituloServicioTarjeta" id="62">Diseño llamativo</h4>
+						<h4 class="TituloServicioTarjeta DisenoLlamativoT" id="62"></h4>
 					</div>
 					<div class="ServicioAtras">
-						<p class="TextoServicio">Mis sitios siempre tendran un diseño atractivo para los usuarios.</p>
+						<p class="TextoServicio" id="TextoServicioT6"></p>
 					</div>
 				</div>
 			</div>
 		</section>
 		<section class="Main__TecnologiasC" id="Tecnologias">
-			<h2 class="TituloSecciones">Tecnologias</h2>
+			<h2 class="TituloSecciones" id="TecnologiasTT"></h2>
 			<div class="ContenedorTecnologias">
 				<div class="Tecnologia">
 					<span></span>
@@ -214,10 +207,10 @@
 					<p class="TituloTecnologiasTarjeta">SQL</p>
 				</div>
 			</div>
-			<h4 class="TextoTecnologias">Cuento con los conocimientos necesarios sobre backend para comunicarme eficientemente con el.</h4>
+			<h4 class="TextoTecnologias" id="TextoTecnologiasT"></h4>
 		</section>
 		<section class="Main__ProyectosC" id="Proyectos">
-			<h2 class="TituloSecciones">Proyectos</h2>
+			<h2 class="TituloSecciones" id="ProyectosTT"></h2>
 			<div class="ContenedorProyecto">
 				<a href="objetosusuarios.com" class="LinkProyecto MrR">
 					<div class="Proyectos">
@@ -232,7 +225,7 @@
 			</div>
 		</section>
 		<section class="Main__ContactoC" id="Contacto">
-			<h2 class="TituloSecciones">Contactame</h2>
+			<h2 class="TituloSecciones" id="ContactoTT"></h2>
 			<form class="FormContacto" method="post">
 				<div class="FormContacto__Iconos">
 					<i class="fal fa-user IconoContacto"></i>
@@ -249,22 +242,22 @@
 					</div>
 				</div>
 				<div class="FormContacto__Info">
-					<label class="Labels" for="Nombre">Nombre *</label>
+					<label class="Labels" for="Nombre" id="NombreT"></label>
 					<div class="GeneralesDiv">
 						<span></span>
 						<span></span>
 						<span></span>
 						<span></span>
-						<input class="Generales" type="text" name="Nombre" placeholder="Nombre" id="Nombre" required="">
+						<input class="Generales" type="text" name="Nombre" id="Nombre" required="" oninvalid="localStorage.getItem('Lengua') == 'MX' ? this.setCustomValidity('Porfavor introduce tu nombre.') : this.setCustomValidity('Please introduce your name.')" oninput="setCustomValidity('')">
 					</div>
 					
-					<label class="Labels" for="Numero">Numero</label>
+					<label class="Labels" for="Numero" id="NumeroT"></label>
 					<div class="GeneralesDiv">
 						<span class="2"></span>
 						<span class="2"></span>
 						<span class="2"></span>
 						<span class="2"></span>
-						<input class="Generales" type="text" name="Numero" placeholder="Numero" id="Numero">
+						<input class="Generales" type="text" name="Numero" placeholder="+1 (123) 456 7890" id="Numero">
 						
 					</div>
 					
@@ -274,32 +267,30 @@
 						<span class="3"></span>
 						<span class="3"></span>
 						<span class="3"></span>
-						<input class="Generales" type="text" name="Email" placeholder="Email" id="Email" required="">
+						<input class="Generales" type="text" name="Email" placeholder="Email" id="Email" required="" oninvalid="localStorage.getItem('Lengua') == 'MX' ? this.setCustomValidity('Porfavor introduce tu email.') : this.setCustomValidity('Please introduce your email.')" oninput="setCustomValidity('')">
 					</div>
 		    	    
-		    	    <label class="Labels" for="Mensaje">Mensaje *</label>
+		    	    <label class="Labels" for="Mensaje" id="MensajeT"></label>
 		    	    <div class="GeneralesDiv Mensaje">
 		    	    	<span class="4"></span>
 						<span class="4"></span>
 						<span class="4"></span>
 						<span class="4"></span>
-		    	    	<textarea class="Generales TextoArriba" name="Mensaje" placeholder="Escribe tu mensaje aqui" id="Mensaje" required=""></textarea> 
+		    	    	<textarea class="Generales TextoArriba" name="Mensaje" id="Mensaje" required="" oninvalid="localStorage.getItem('Lengua') == 'MX' ? this.setCustomValidity('Porfavor escribe tu mensaje.') : this.setCustomValidity('Please type your message.')" oninput="setCustomValidity('')"></textarea> 
 		    	    </div>
 		    	    
-		       		<button class="Submit" type="submit" name="Contactar">Mandar mensaje</button>
+		       		<button class="Submit" type="submit" name="Contactar" id="SubmitT"></button>
 				</div>
 			</form>
 		</section>
 	</main>
 
 	<footer class="Footer">
-		<button class="Resume">Resume</button>
-		<div class="FooterDiv">
-			Copyright © 2021 <br>
-			Todos los derechos reservados
+		<button class="Resume" id="CurriculumT"></button>
+		<div class="FooterDiv" id="Copyright">
+			Copyright © 2022 <br>
+			Todos los derechos reservados.
 		</div>
-
-		
 	</footer>
 	<script type="text/javascript" src="JavaScript/Js.js"></script>
 </body>
